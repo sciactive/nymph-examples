45abfdaf982215a021248ba77537f08bf5df0fd8
Hunter Perrin <hperrin@gmail.com>
2015-04-08 20:26:35 -0700

    Update for new Nymph changes.

--- examples/config.php
+++ examples/config.php
@@ -1,30 +1,37 @@
 <?php
+
 // Nymph's configuration.
 
-$nymph_config = include file_exists(dirname(dirname(__DIR__)).'/server/conf/defaults.php') ? dirname(dirname(__DIR__)).'/server/conf/defaults.php' : dirname(__DIR__).'/vendor/sciactive/nymph-server/conf/defaults.php';
+$nymphConfig = [];
 
 // Check for Heroku postgres var.
 if (getenv('DATABASE_URL')) {
 	$dbopts = parse_url(getenv('DATABASE_URL'));
-	$nymph_config->driver['value'] = 'PostgreSQL';
-	$nymph_config->PostgreSQL->database['value'] = ltrim($dbopts["path"],'/');
-	$nymph_config->PostgreSQL->host['value'] = $dbopts["host"];
-	$nymph_config->PostgreSQL->port['value'] = $dbopts["port"];
-	$nymph_config->PostgreSQL->user['value'] = $dbopts["user"];
-	$nymph_config->PostgreSQL->password['value'] = $dbopts["pass"];
+	$nymphConfig['driver'] = 'PostgreSQL';
+	$nymphConfig['PostgreSQL'] = [
+		'database' => ltrim($dbopts["path"], '/'),
+		'host' => $dbopts["host"],
+		'port' => $dbopts["port"],
+		'user' => $dbopts["user"],
+		'password' => $dbopts["pass"]
+	];
 } else {
 	if (true) {
-		$nymph_config->MySQL->host['value'] = '127.0.0.1';
-		$nymph_config->MySQL->database['value'] = 'nymph_example';
-		$nymph_config->MySQL->user['value'] = 'nymph_example';
-		$nymph_config->MySQL->password['value'] = 'omgomg';
+		$nymphConfig['MySQL'] = [
+			'host' => '127.0.0.1',
+			'database' => 'nymph_example',
+			'user' => 'nymph_example',
+			'password' => 'omgomg'
+		];
 	} else {
-		$nymph_config->driver['value'] = 'PostgreSQL';
-		$nymph_config->PostgreSQL->host['value'] = '127.0.0.1';
-		$nymph_config->PostgreSQL->database['value'] = 'nymph_example';
-		$nymph_config->PostgreSQL->user['value'] = 'nymph_example';
-		$nymph_config->PostgreSQL->password['value'] = 'omgomg';
+		$nymphConfig['driver'] = 'PostgreSQL';
+		$nymphConfig['PostgreSQL'] = [
+			'host' => '127.0.0.1',
+			'database' => 'nymph_example',
+			'user' => 'nymph_example',
+			'password' => 'omgomg'
+		];
 	}
 }
 
-return $nymph_config;
+\Nymph\Nymph::configure($nymphConfig);
