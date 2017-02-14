8b18fb24858559ba557522513a1c4cda91af4b80
Hunter Perrin <hperrin@gmail.com>
2017-02-14 10:24:11 -0800

    Some code cleanup. Preparing for PHP7.

--- examples/pubsub.php
+++ examples/pubsub.php
@@ -2,7 +2,9 @@
 
 error_reporting(E_ALL);
 
-require file_exists(__DIR__.'/../../autoload-dev.php') ? __DIR__.'/../../autoload-dev.php' : __DIR__.'/../vendor/autoload.php';
+require file_exists(__DIR__.'/../../autoload-dev.php')
+    ? __DIR__.'/../../autoload-dev.php'
+    : __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/config.php';
 
 date_default_timezone_set('America/Los_Angeles');
