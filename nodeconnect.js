8d8b8f20276c16ca174e7cd324e8a5595a65ecd9
Hunter Perrin <hperrin@gmail.com>
2017-10-14 16:38:45 -0700

    Using new Node client in the example.

--- nodeconnect.js
+++ nodeconnect.js
@@ -1,46 +1,39 @@
-// Do aliases to get around Node modules.
-const alias = require('module-alias');
-alias.addAliases({
-  'Nymph': __dirname + '/../client/lib-cjs/Nymph',
-  'NymphEntity': __dirname + '/../client/lib-cjs/Entity',
-  'NymphPubSub': __dirname + '/../client/lib-cjs/PubSub',
-  'Entity': __dirname + '/../client/lib-cjs/Entity',
-  'PubSub': __dirname + '/../client/lib-cjs/PubSub',
-  'Todo': __dirname + '/examples/todo/Todo.cjs',
-});
-
-// Set up some shit that Nymph needs.
-global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-global.WebSocket = require('websocket').w3cwebsocket;
-
+// Nymph Node client for the win.
+const Nymph = require('nymph-client-node');
 // Set up Nymph.
 const nymphOptions = {
   restURL: 'http://nymph-demo.herokuapp.com/examples/rest.php',
   pubsubURL: 'ws://nymph-pubsub-demo.herokuapp.com:80',
   rateLimit: 100
 };
-const Nymph = require('Nymph').Nymph;
 Nymph.init(nymphOptions);
-const PubSub = require('NymphPubSub').PubSub;
-PubSub.init(nymphOptions);
-const Todo = require('Todo').Todo;
+const Todo = require('./examples/todo/Todo.cjs').Todo;
 
 
-// Make a new todo, wait 2 seconds, set it to done.
 main();
 async function main() {
-  const todo = new Todo();
-  todo.set("name", "I am a new todo from Node.");
-  console.log("await todo.save(): ", await todo.save());
-
-  await new Promise((r) => setTimeout(() => r(), 2000));
-
-  todo.set("done", true);
-  console.log("await todo.save(): ", await todo.save());
-
+  // Listen to Foobar entities, and show them on the console.
   Nymph.getEntities({'class': Todo.class}, {'type': '&', 'strict': ['name', 'Foobar']}).subscribe((todos) => {
-    console.log("\n\nNew Todo Updates: ", todos);
+    console.log("\n\nReceived Todo Updates: ", todos);
   });
+
+  // Make a new todo.
+  const todo = new Todo();
+  todo.set("name", "Foobar");
+  console.log("\n\nawait todo.save(): ", await todo.save());
+
+  // Wait 2 seconds, set it to done.
+  await new Promise((r) => setTimeout(() => r(), 2000));
+  todo.set("done", true);
+  console.log("\n\nawait todo.save(): ", await todo.save());
+
+  // Wait 2 seconds, archive it.
+  await new Promise((r) => setTimeout(() => r(), 2000));
+  console.log("\n\nawait todo.archive(): ", await todo.archive());
+
+  // Wait 2 seconds, delete it.
+  await new Promise((r) => setTimeout(() => r(), 2000));
+  console.log("\n\nawait todo.delete(): ", await todo.delete());
 
   return;
 }
