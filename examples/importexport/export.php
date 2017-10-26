d2ce3b9ff42b1fd4d59e7327bc575da0409d5812
Hunter Perrin <hperrin@gmail.com>
2017-10-25 22:21:47 -0700

    Enable export/import for Heroku.

--- examples/importexport/export.php
+++ examples/importexport/export.php
@@ -1,11 +1,11 @@
 <?php
 
-if (getenv('DATABASE_URL')) {
-  // No import/export on Heroku.
-  header('HTTP/1.1 403 Forbidden', true, 403);
-  echo "403 Forbidden";
-  exit;
-}
+// if (getenv('DATABASE_URL')) {
+//   // No import/export on Heroku.
+//   header('HTTP/1.1 403 Forbidden', true, 403);
+//   echo "403 Forbidden";
+//   exit;
+// }
 
 require file_exists(__DIR__.'/../../../autoload-dev.php')
     ? __DIR__.'/../../../autoload-dev.php'
