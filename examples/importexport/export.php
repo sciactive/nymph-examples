69051577878f6320dbfd169f221c5458ca7fbbbd
Hunter Perrin <hperrin@gmail.com>
2019-06-03 11:57:50 -0700

    Allow export.

--- examples/importexport/export.php
+++ examples/importexport/export.php
@@ -5,12 +5,12 @@ require file_exists(__DIR__.'/../../../autoload-dev.php')
     : __DIR__.'/../../vendor/autoload.php';
 require __DIR__.'/../config-tilmeld.php';
 
-if (!\Tilmeld\Tilmeld::gatekeeper('system/admin')) {
-  // No import/export on production.
-  header('HTTP/1.1 403 Forbidden', true, 403);
-  echo "403 Forbidden";
-  exit;
-}
+// if (!\Tilmeld\Tilmeld::gatekeeper('system/admin')) {
+//   // No import/export on production.
+//   header('HTTP/1.1 403 Forbidden', true, 403);
+//   echo "403 Forbidden";
+//   exit;
+// }
 
 require '../employee/Employee.php';
 require '../sudoku/Game.php';
