74e386cff9ad67cbf91278d33c5a5379d27a4e36
Hunter Perrin <hperrin@gmail.com>
2019-06-10 17:26:56 -0700

    Update client test demo.

--- examples/test/Employee.src.js
+++ examples/test/Employee.src.js
@@ -1,34 +1,34 @@
 // This file is a demo class that extends the Entity class.
 
-import {Nymph, Entity} from 'nymph-client';
+import { Nymph, Entity } from "nymph-client";
 
 export class Employee extends Entity {
   constructor(id) {
     super(id);
-    this.addTag('employee');
-    this.data.current = true;
-    this.data.subordinates = [];
+    this.$addTag("employee");
+    this.current = true;
+    this.subordinates = [];
   }
 
-  throwError(...args) {
-    return this.serverCall('throwError', args);
+  $throwError(...args) {
+    return this.$serverCall("throwError", args);
   }
 
   static testStatic(value, ...args) {
-    return Employee.serverCallStatic('testStatic', [value, ...args]);
+    return Employee.serverCallStatic("testStatic", [value, ...args]);
   }
 
   static throwErrorStatic(...args) {
-    return Employee.serverCallStatic('throwErrorStatic', args);
+    return Employee.serverCallStatic("throwErrorStatic", args);
   }
 
   static inaccessibleMethod(...args) {
-    return Employee.serverCallStatic('inaccessibleMethod', args);
+    return Employee.serverCallStatic("inaccessibleMethod", args);
   }
 }
 
 // The name of the server class
-Employee.class = 'Employee';
+Employee.class = "Employee";
 
 Nymph.setEntityClass(Employee.class, Employee);
 export default Employee;
